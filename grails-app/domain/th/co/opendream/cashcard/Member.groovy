e414173c271ef631a5c82e264f1463470835b430
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2012-02-23 16:08:00 +0700

    Fix indentation.

--- grails-app/domain/th/co/opendream/cashcard/Member.groovy
+++ grails-app/domain/th/co/opendream/cashcard/Member.groovy
@@ -1,29 +1,29 @@
 package th.co.opendream.cashcard
 
 class Member {
-	String identificationNumber
-	String firstname
-	String lastname
-	Gender gender
+    String identificationNumber
+    String firstname
+    String lastname
+    Gender gender
     String address
     String telNo
     BigDecimal balance = 0.00
     BigDecimal interest = 0.00
-	Date dateCreated
+    Date dateCreated
     Date lastUpdated
 
-   	public enum Gender {
-   		MALE,
-   		FEMALE
-   		static list() {
-			 [MALE, FEMALE]
-		  }
-   	}
+    public enum Gender {
+      MALE,
+      FEMALE
+      static list() {
+       [MALE, FEMALE]
+      }
+    }
 
     static constraints = {
-    	identificationNumber(blank: false, unique: true, matches: /\d{13}/)
-    	firstname(blank: false)
-    	lastname(blank: false)
+        identificationNumber(blank: false, unique: true, matches: /\d{13}/)
+        firstname(blank: false)
+        lastname(blank: false)
         address(blank: false)
         telNo(nullable:true, matches: /\d{9,11}/)
         balance(blank: true, nullable: true)
