2cbc71cf3572a4458fdebebb7bd221d3a21e3905
Worawat Yao <worawatyao@gmail.com>
2012-03-14 15:16:40 +0700

    service สร้างตารางใน postgres ให้กับ schema ของ company

--- grails-app/domain/th/co/opendream/cashcard/Company.groovy
+++ grails-app/domain/th/co/opendream/cashcard/Company.groovy
@@ -1,10 +1,17 @@
 package th.co.opendream.cashcard
 
 class Company {
+    Long id
     String name
     String address
     String taxId
 
+    String getSchema() {
+        "c$id"
+    }
+
+    static transients = ['schema']
+
     static hasMany = [users:Users, members:Member]
-    
+
 }
