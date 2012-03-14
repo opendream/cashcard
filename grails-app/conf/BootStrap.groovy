2cae9ae4d467cb50971b655a970d1af79263f631
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-03-14 17:49:48 +0700

    refactor          - create company and schema by companyService

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -15,14 +15,16 @@ import static java.util.Calendar.*
 
 
 class BootStrap {
-
+    def companyService
+    def schemaService
     def init = { servletContext ->
         def opendream = new Company(name:'opendream', address:'bkk', taxId:'1-2-3-4')
-        def user = new Users(username:'admin', password:'openpubyesroti!', 
-                            firstname:'admin', lastname:'messenger',
-                            email:'admin@messenger.opendream.org', enabled:true)
+        def user = new Users(username:'admin', password:'password', 
+                            , enabled:true, accountExpired:false, accountLocked:false, passwordExpired:false)
+        //user.save()
         opendream.addToUsers(user)
-        opendream.save(failOnError: true)
+        companyService.save(opendream)
+        //opendream.save(failOnError: true)
         def role = new Role(authority:'ROLE_ADMIN').save(failOnError: true)
         new UsersRole(user:user, role:role).save(failOnError: true)
 
@@ -32,27 +34,20 @@ class BootStrap {
     	m1.save()
     	m2.save()
 
-
-
-    	new Policy(key: Policy.KEY_CREDIT_LINE, value: 2000).save()
-    	new Policy(key: Policy.KEY_INTEREST_METHOD, value: Policy.VALUE_NON_COMPOUND).save()
-    	new Policy(key: Policy.KEY_INTEREST_RATE_LIMIT, value: '18.00').save()
-
         def today = Calendar.instance
-        today = today.time
+            today = today.time
 
-        new InterestTransaction([id: 1, member: m1, amount: 10.00, txType: TransactionType.CREDIT, date: today, fee: 0.00, interest: 10.00]).save()
-        new InterestTransaction([id: 2, member: m2, amount: 21.00, txType: TransactionType.CREDIT, date: today, fee: 0.00, interest: 21.00]).save()
-        new InterestTransaction([id: 3, member: m1, amount: 13.00, txType: TransactionType.CREDIT, date: today.plus(1), fee: 0.00, interest: 13.00]).save()
-        new InterestTransaction([id: 4, member: m2, amount: 24.00, txType: TransactionType.CREDIT, date: today.plus(1), fee: 0.00, interest: 24.00]).save()
-        
-        new BalanceTransaction([id: 1, member: m1, amount: 500.00, txType: TransactionType.CREDIT, activity: ActivityType.WITHDRAW, date: today, net: 500.00, remainder: 0.00]).save()
-        new BalanceTransaction([id: 2, member: m2, amount: 700.00, txType: TransactionType.CREDIT, activity: ActivityType.WITHDRAW, date: today, net: 700.00, remainder: 0.00]).save()
-        new BalanceTransaction([id: 3, member: m1, amount: 200.00, txType: TransactionType.CREDIT, activity: ActivityType.WITHDRAW, date: today.plus(1), net: 200.00, remainder: 0.00]).save()
-        new BalanceTransaction([id: 4, member: m2, amount: 300.00, txType: TransactionType.DEBIT, activity: ActivityType.PAYMENT, date: today.plus(1), net: 300.00, remainder: 0.00]).save()
-        new BalanceTransaction([id: 5, member: m1, amount: 707.50, txType: TransactionType.DEBIT, activity: ActivityType.PAYMENT, date: today.plus(2), net: 707.32, remainder: 0.18]).save()
-        new BalanceTransaction([id: 6, member: m2, amount: 405.25, txType: TransactionType.DEBIT, activity: ActivityType.PAYMENT, date: today.plus(2), net: 405.08, remainder: 0.17]).save()
-    
+            new InterestTransaction([id: 1, member: m1, amount: 10.00, txType: TransactionType.CREDIT, date: today, fee: 0.00, interest: 10.00]).save()
+            new InterestTransaction([id: 2, member: m2, amount: 21.00, txType: TransactionType.CREDIT, date: today, fee: 0.00, interest: 21.00]).save()
+            new InterestTransaction([id: 3, member: m1, amount: 13.00, txType: TransactionType.CREDIT, date: today.plus(1), fee: 0.00, interest: 13.00]).save()
+            new InterestTransaction([id: 4, member: m2, amount: 24.00, txType: TransactionType.CREDIT, date: today.plus(1), fee: 0.00, interest: 24.00]).save()
+            
+            new BalanceTransaction([id: 1, member: m1, amount: 500.00, txType: TransactionType.CREDIT, activity: ActivityType.WITHDRAW, date: today, net: 500.00, remainder: 0.00]).save()
+            new BalanceTransaction([id: 2, member: m2, amount: 700.00, txType: TransactionType.CREDIT, activity: ActivityType.WITHDRAW, date: today, net: 700.00, remainder: 0.00]).save()
+            new BalanceTransaction([id: 3, member: m1, amount: 200.00, txType: TransactionType.CREDIT, activity: ActivityType.WITHDRAW, date: today.plus(1), net: 200.00, remainder: 0.00]).save()
+            new BalanceTransaction([id: 4, member: m2, amount: 300.00, txType: TransactionType.DEBIT, activity: ActivityType.PAYMENT, date: today.plus(1), net: 300.00, remainder: 0.00]).save()
+            new BalanceTransaction([id: 5, member: m1, amount: 707.50, txType: TransactionType.DEBIT, activity: ActivityType.PAYMENT, date: today.plus(2), net: 707.32, remainder: 0.18]).save()
+            new BalanceTransaction([id: 6, member: m2, amount: 405.25, txType: TransactionType.DEBIT, activity: ActivityType.PAYMENT, date: today.plus(2), net: 405.08, remainder: 0.17]).save()   
     }
 
     def destroy = {
