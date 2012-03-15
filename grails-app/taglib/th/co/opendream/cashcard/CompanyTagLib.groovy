601cc085a86cb6266de5d7dfdd33605433d235cf
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-03-15 14:17:50 +0700

    remove unusedcode in CompanyTagLib

--- grails-app/taglib/th/co/opendream/cashcard/CompanyTagLib.groovy
+++ grails-app/taglib/th/co/opendream/cashcard/CompanyTagLib.groovy
@@ -5,8 +5,6 @@ class CompanyTagLib {
 	def springSecurityService
 	
 	def name = { 
-		println "principal " + springSecurityService?.principal
-		println "companyName " +springSecurityService?.principal?.companyName
 		out << springSecurityService?.principal?.companyName
 	}
 }
