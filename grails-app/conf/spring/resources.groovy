78b21fead89d519d8f05608137500a22426791c2
Nat <nat.weerawan@gmail.com>
2012-03-15 18:05:40 +0700

    Set project default language to Thai

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,4 +1,8 @@
 beans = {
     userDetailsService(th.co.opendream.cashcard.CompanyUserDetailsService)
+    localeResolver(org.springframework.web.servlet.i18n.SessionLocaleResolver) {
+        defaultLocale = new Locale("th","TH")
+        java.util.Locale.setDefault(defaultLocale)
+    }
 }
 
