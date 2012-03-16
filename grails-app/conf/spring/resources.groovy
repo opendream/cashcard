ef419a01c6f93a61dcd45b5558c5f95224257001
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-03-16 19:56:40 +0700

    add default locale to TH

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,4 +1,5 @@
 beans = {
     userDetailsService(th.co.opendream.cashcard.CompanyUserDetailsService)
+    localeResolver (org.springframework.web.servlet.i18n.FixedLocaleResolver, new Locale("th","TH")) 
 }
 
