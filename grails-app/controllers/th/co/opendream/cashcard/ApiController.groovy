d237d3d24ec4c8eceea26f68cdd88c7717341423
Nat <nat.weerawan@gmail.com>
2012-03-29 09:55:33 +0700

    [CASHCARD-64] Change query method from GORM to sql.execute directly

--- grails-app/controllers/th/co/opendream/cashcard/ApiController.groovy
+++ grails-app/controllers/th/co/opendream/cashcard/ApiController.groovy
@@ -1,10 +1,12 @@
 package th.co.opendream.cashcard
 
 import grails.converters.JSON
+import org.springframework.transaction.support.TransactionSynchronizationManager
+import grails.util.Environment
 
 class ApiController {
 	def schemaService
-
+    def sessionFactory
 
     def index() { }
 
@@ -12,10 +14,23 @@ class ApiController {
     	println "F"
     	println params
     	def c
-		schemaService.with(params.companySchema) {
-	    	c = BalanceTransaction.list()
-			render c as JSON
-			return
-    	}
+
+		//schemaService.with(params.companySchema) {
+	    //	c = BalanceTransaction.list()
+		//	render c as JSON
+		//	return
+        def holder = TransactionSynchronizationManager.getResource(sessionFactory)
+        def session = holder.getSession();
+        def conn = session.connection();
+        groovy.sql.Sql sql = new groovy.sql.Sql(conn)
+
+        // TODO: Prevent SQL injection in GString. (include null or empty string)
+        def transactionHistory = []
+        def query = "SELECT * FROM " + params.companySchema + ".transaction"
+        println query
+        sql.eachRow(query) {
+            transactionHistory << it.toRowResult()
+        }
+        render transactionHistory as JSON
     }
 }
