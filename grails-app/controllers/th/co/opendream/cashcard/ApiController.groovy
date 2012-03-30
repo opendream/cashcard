95bf381e93b5bb3bc9fec40a0ef521ead4d5997f
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2012-03-30 12:22:42 +0700

    [CASHCARD-64] API: Revert back to manual query method

--- grails-app/controllers/th/co/opendream/cashcard/ApiController.groovy
+++ grails-app/controllers/th/co/opendream/cashcard/ApiController.groovy
@@ -5,17 +5,32 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
 import grails.util.Environment
 
 class ApiController {
+    def sessionFactory
 	def schemaService
 
     def index() { }
 
-    def getTransactionHistory() {
+    def getMemberTransactionHistory() {
         def member = Member.get(params.memberId as Integer)
-    	def c
 
-		schemaService.with(member.company.getSchema()) {
-	    	c = member.balanceTransactions.sort { it.date }
-			render c as JSON
+        def holder = TransactionSynchronizationManager.getResource(sessionFactory)
+        def session = holder.getSession();
+        def conn = session.connection();
+        groovy.sql.Sql sql = new groovy.sql.Sql(conn)
+
+        // TODO: Prevent SQL injection in GString. (include null or empty string)
+        def transactionHistory = []
+        def schema = member.company.getSchema()
+        def query = "\
+            SELECT * \
+            FROM " + schema + ".transaction\
+            WHERE member_id = " + member.id + "\
+            ORDER BY date ASC\
+        "
+
+        sql.eachRow(query) {
+            transactionHistory << it.toRowResult()
         }
+        render transactionHistory as JSON
     }
 }
