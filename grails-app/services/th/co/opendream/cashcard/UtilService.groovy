1507cdce87068eb6cd4ceffc01afdf7b16bbdafc
Nat <nat.weerawan@gmail.com>
2012-04-23 16:55:44 +0700

    Add test.\

--- grails-app/services/th/co/opendream/cashcard/UtilService.groovy
+++ grails-app/services/th/co/opendream/cashcard/UtilService.groovy
@@ -5,4 +5,8 @@ class UtilService {
     static def moneyRoundUp(amount) {
 		[0.00, 0.25, 0.50, 0.75, 1.00].collect { it + (amount as BigInteger) }.find { it >= amount }
     }
+
+    static def interestRateEditable(InterestRate rate) {
+    	return false
+    }
 }
--- test/unit/th/co/opendream/cashcard/UtilServiceTests.groovy
+++ test/unit/th/co/opendream/cashcard/UtilServiceTests.groovy
@@ -1,15 +1,24 @@
 package th.co.opendream.cashcard
 
-
-
 import grails.test.mixin.*
 import org.junit.*
+import org.codehaus.groovy.runtime.typehandling.GroovyCastException
+import groovy.time.*
+import static java.util.Calendar.*
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(UtilService)
+@Mock([InterestRate])
 class UtilServiceTests {
+    def today
+    @Before
+    void setUp() {
+        today = Calendar.instance
+        today.set 2012, JANUARY, 1
+        today = today.time
+    }
 
     void testMoneyRoundUp() {
     	assert service.moneyRoundUp(0.00) == 0.00
@@ -17,5 +26,33 @@ class UtilServiceTests {
     	assert service.moneyRoundUp(100.34) == 100.50
     	assert service.moneyRoundUp(100.73) == 100.75
     	assert service.moneyRoundUp(100.84) == 101.00
+    }
+
+    void testInterestRateEditable() {
+        def currentDate = today
+    	def existingRate = new InterestRate(
+            startDate: currentDate,
+            endDate: use(TimeCategory) { currentDate + 10.days },
+            rate: 5.00F
+        )
+
+        // FAIL
+        assert service.interestRateEditable(existingRate) == false
+
+    	existingRate = new InterestRate(
+            startDate: currentDate,
+            endDate: use(TimeCategory) { currentDate + 10.days },
+            rate: 5.00F
+        )
+
+        Calendar.metaClass.static.getInstance = {
+        	use(TimeCategory) {
+	        	today = today - 1.days
+	        }
+            today.toCalendar()
+        }
+        // PASS
+        assert service.interestRateEditable(existingRate) == true
+
     }
 }
