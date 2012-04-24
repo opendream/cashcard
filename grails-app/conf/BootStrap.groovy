ad016cb93394cfaa5bfc37d7823a7001799c8cd3
Siriwat Uamngamsup <lazym0m3nt@gmail.com>
2012-04-24 19:19:03 +0700

    [CASHCARD-76] Clean and fix bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -12,55 +12,57 @@ import th.co.opendream.cashcard.ActivityType
 import th.co.opendream.cashcard.RequestMap
 import groovy.time.*
 import static java.util.Calendar.*
-
+import grails.util.Environment
 
 
 class BootStrap {
     def companyService
     def schemaService
     def init = { servletContext ->
-        def opendream = new Company(name:'opendream', address:'bkk', taxId:'1-2-3-4')
-        def opendreamx = new Company(name:'opendreamx', address:'bkk', taxId:'1-2-3-4')
-        def user = new Users(username:'admin', password:'password', 
-                            , enabled:true, accountExpired:false, accountLocked:false, passwordExpired:false)
+        if (Environment.currentEnvironment == Environment.DEVELOPMENT ||
+            Environment.currentEnvironment == Environment.TEST) {
 
-    	def m1 = new Member(identificationNumber:"1159900100015", firstname:"Nat", lastname: "Weerawan", telNo: "111111111", gender: "MALE", address: "Opendream")
-    	def m2 = new Member(identificationNumber: "1234567891234", firstname: "Noomz", lastname: "Siriwat", telNo: "111111111", gender: "MALE", address: "Opendream2")
+            def opendream = new Company(name:'opendream', address:'bkk', taxId:'1-2-3-4')
+            def policy1 = new Policy(key: Policy.KEY_CREDIT_LINE, value: "2000.00", company: opendream)
+            def policy2 = new Policy(key: Policy.KEY_INTEREST_METHOD, value: Policy.VALUE_NON_COMPOUND)
+            def policy3 = new Policy(key: Policy.KEY_INTEREST_RATE_LIMIT, value: '18.00')
 
-    	//m1.save()
-    	//m2.save()
-        //user.save()
-        opendream.addToUsers(user)
-        opendream.addToMembers(m1)        
-        companyService.save(opendream)
+            opendream
+                .addToPolicy(policy1)
+                .addToPolicy(policy2)
+                .addToPolicy(policy3)
 
-        opendreamx.addToMembers(m2)
-        companyService.save(opendreamx)
+            def opendream2 = new Company(name:'opendream2', address:'bkk', taxId:'1-2-3-4')
+            def policy4 = new Policy(key: Policy.KEY_CREDIT_LINE, value: "4000.00", company: opendream)
+            def policy5 = new Policy(key: Policy.KEY_INTEREST_METHOD, value: Policy.VALUE_COMPOUND)
+            def policy6 = new Policy(key: Policy.KEY_INTEREST_RATE_LIMIT, value: '18.00')
+            opendream2
+                .addToPolicy(policy4)
+                .addToPolicy(policy5)
+                .addToPolicy(policy6)
 
-        //opendream.save(failOnError: true)
-        def roleAdmin = new Role(authority:'ROLE_ADMIN').save(failOnError: true)
-        def roleCounter = new Role(authority:'ROLE_COUNTER').save(failOnError: true)
-        def roleUser = new Role(authority:'ROLE_USER').save(failOnError: true)
-        new UsersRole(user:user, role:roleAdmin).save(failOnError: true)
-        new UsersRole(user:user, role:roleCounter).save(failOnError: true)
-        new UsersRole(user:user, role:roleUser).save(failOnError: true)
+            def user = new Users(username:'admin', password:'password', 
+                                , enabled:true, accountExpired:false, accountLocked:false, passwordExpired:false)
 
-        def today = Calendar.instance
-            today = today.time
+        	def m1 = new Member(identificationNumber:"1159900100015", firstname:"Nat", lastname: "Weerawan", telNo: "111111111", gender: "MALE", address: "Opendream")
+        	def m2 = new Member(identificationNumber: "1234567891234", firstname: "Noomz", lastname: "Siriwat", telNo: "111111111", gender: "MALE", address: "Opendream2")
 
-            /*new InterestTransaction([id: 1, member: m1, amount: 10.00, txType: TransactionType.CREDIT, date: today, fee: 0.00, interest: 10.00]).save()
-            new InterestTransaction([id: 2, member: m2, amount: 21.00, txType: TransactionType.CREDIT, date: today, fee: 0.00, interest: 21.00]).save()
-            new InterestTransaction([id: 3, member: m1, amount: 13.00, txType: TransactionType.CREDIT, date: today.plus(1), fee: 0.00, interest: 13.00]).save()
-            new InterestTransaction([id: 4, member: m2, amount: 24.00, txType: TransactionType.CREDIT, date: today.plus(1), fee: 0.00, interest: 24.00]).save()
-            
-            new BalanceTransaction([id: 1, member: m1, amount: 500.00, txType: TransactionType.CREDIT, activity: ActivityType.WITHDRAW, date: today, net: 500.00, remainder: 0.00]).save()
-            new BalanceTransaction([id: 2, member: m2, amount: 700.00, txType: TransactionType.CREDIT, activity: ActivityType.WITHDRAW, date: today, net: 700.00, remainder: 0.00]).save()
-            new BalanceTransaction([id: 3, member: m1, amount: 200.00, txType: TransactionType.CREDIT, activity: ActivityType.WITHDRAW, date: today.plus(1), net: 200.00, remainder: 0.00]).save()
-            new BalanceTransaction([id: 4, member: m2, amount: 300.00, txType: TransactionType.DEBIT, activity: ActivityType.PAYMENT, date: today.plus(1), net: 300.00, remainder: 0.00]).save()
-            new BalanceTransaction([id: 5, member: m1, amount: 707.50, txType: TransactionType.DEBIT, activity: ActivityType.PAYMENT, date: today.plus(2), net: 707.32, remainder: 0.18]).save()
-            new BalanceTransaction([id: 6, member: m2, amount: 405.25, txType: TransactionType.DEBIT, activity: ActivityType.PAYMENT, date: today.plus(2), net: 405.08, remainder: 0.17]).save() */  
+            opendream.addToUsers(user)
+            opendream.addToMembers(m1)     
+            companyService.save(opendream)
 
-            createRequestMaps();
+            opendream2.addToMembers(m2)
+            companyService.save(opendream2)
+
+            def roleAdmin = new Role(authority:'ROLE_ADMIN').save(failOnError: true)
+            def roleCounter = new Role(authority:'ROLE_COUNTER').save(failOnError: true)
+            def roleUser = new Role(authority:'ROLE_USER').save(failOnError: true)
+            new UsersRole(user:user, role:roleAdmin).save(failOnError: true)
+            new UsersRole(user:user, role:roleCounter).save(failOnError: true)
+            new UsersRole(user:user, role:roleUser).save(failOnError: true) 
+        }
+
+        createRequestMaps();
     }
 
     def destroy = {
