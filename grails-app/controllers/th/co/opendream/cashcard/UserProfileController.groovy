2e99a119a76e05cd92037f62f6afb0a71ee42470
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-04-26 14:20:44 +0700

    finished implement UserProfileController

---
+++ grails-app/controllers/th/co/opendream/cashcard/UserProfileController.groovy
@@ -0,0 +1,38 @@
+package th.co.opendream.cashcard
+
+class UserProfileController {
+
+	def springSecurityService
+
+    def index() { }
+
+    def show() {
+    	def userId = springSecurityService.principal.id
+    	def model = Users.get(userId)
+    	model
+    }
+
+    def edit() {
+    	def userId = springSecurityService.principal.id
+    	def model = Users.get(userId)
+    	model
+    }
+
+    def update() {
+    	def userId = springSecurityService.principal.id
+    	def user = Users.get(userId)
+    	def oldpassword = user.password
+    	def password = params.password
+
+    	if(oldpassword != springSecurityService.encodePassword(password)) {
+    		user.password = password
+    	}
+
+    	if(!user.save(flush: true)) {
+			redirect action: 'edit'
+			return
+		}
+
+    	redirect action: 'show'
+    }
+}
