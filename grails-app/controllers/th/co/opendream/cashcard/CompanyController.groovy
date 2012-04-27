8734305d4dc91537c46a39e2579570427646fb85
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2012-04-27 20:11:11 +0700

    add create and edit policy in company

--- grails-app/controllers/th/co/opendream/cashcard/CompanyController.groovy
+++ grails-app/controllers/th/co/opendream/cashcard/CompanyController.groovy
@@ -14,35 +14,60 @@ class CompanyController {
 
     def save() {
     	def company = new Company(params)
-    	try {			
-			companyService.save(company)
+    	try {		
+    		def policies = validatePolicy()    		
+    		if(policies) {
+    			policies.each {
+    				company.addToPolicy(it)
+    			}
+    		}
+    		
+    		companyService.save(company)
 			flash.message = "${message(code: 'default.created.message', args: [message(code: 'company.label', default: 'Company'), company.id])}"
-			redirect action: 'edit', id: company.id
+			redirect action: 'edit', id: company.id 
+			
 		} catch(e) {
 			log.error e
-			render view: 'create', model: [company: company]	         
+			render view: 'create', model: [company: company, policyKeyInterestMethod:policyKeyInterestMethod, policyKeyInterestRateLimit:policyKeyInterestRateLimit, policyKeyCreditLine:policyKeyCreditLine]	         
 		}
 	}
 
     def edit() {
     	def company = params.name ? Company.findByName(params.name) : null
+    	def policyKeyInterestMethod
+    	def policyKeyInterestRateLimit
+    	def policyKeyCreditLine
+
     	if(!company) {
     		company = Company.get(params.id)
+    		def policies = company.policy
+    		policyKeyInterestMethod = policies.find { it.key == 'InterestMethod'}.value    		
+    		policyKeyInterestRateLimit = policies.find { it.key == 'InterestRateLimit'}.value
+    		policyKeyCreditLine = policies.find { it.key == 'CreditLine'}.value
     	}
     	if(!company) {
     		return
     	}
-    	[company:company, users:company.users, members:company.members]
+    	[company:company, users:company.users, members:company.members, policyKeyInterestMethod:policyKeyInterestMethod, policyKeyInterestRateLimit:policyKeyInterestRateLimit, policyKeyCreditLine:policyKeyCreditLine]
     }
 
     def update() {
 		def company = Company.get(params.id)
 		if (!company) return
-		if (company.version > params.version) {
+		if (company.version > params.long('version')) {
 			return
 		}
 		company.properties = params
+		def policies = validatePolicy()    		
+    		if(policies) {    			
+    			policies.each { newPolicy ->
+    				def currentPolicy = company.policy.find { it.key == newPolicy.key}
+    				currentPolicy.value = newPolicy.value    				
+    			}
+    		}
+
 		if (!company.save(flush:true)) { 
+			println 'fail'
 			render view: 'edit', model: [company:company, users:company.users, members:company.members]
 			return
 		}
@@ -76,5 +101,18 @@ class CompanyController {
 		}
 
 		render view: 'search', model: model
+	}
+
+	protected def validatePolicy() {
+		def policies
+		if( params.policyKeyInterestMethod && params.float('policyKeyInterestRateLimit') &&
+    		params.float('policyKeyCreditLine') ) { 
+    		policies = []
+    		policies.add (new Policy(key: Policy.KEY_CREDIT_LINE, value: params.policyKeyCreditLine))
+            policies.add (new Policy(key: Policy.KEY_INTEREST_METHOD, value: params.policyKeyInterestMethod))
+            policies.add (new Policy(key: Policy.KEY_INTEREST_RATE_LIMIT, value: params.policyKeyInterestRateLimit))	
+
+    	}
+    	policies
 	}
 }
--- grails-app/views/company/edit.gsp
+++ grails-app/views/company/edit.gsp
@@ -1,3 +1,4 @@
+<%@ page import="th.co.opendream.cashcard.Policy" %>
 <html>
 
 <head>
@@ -21,9 +22,9 @@ tabData << [name: 'users',    icon: 'icon_users', messageCode: 'spring.security.
 tabData << [name: 'members',    icon: 'icon_users', messageCode: 'spring.security.ui.company.members']
 %>
 
-<s2ui:tabs elementId='tabs' height='150' data="${tabData}">
+<s2ui:tabs elementId='tabs' height='300' data="${tabData}">
 
-	<s2ui:tab name='info' height='150'>
+	<s2ui:tab name='info' height='250'>
 		<table>
 		<tbody>
 			<s2ui:textFieldRow name='name' labelCode='company.name.label' bean="${company}"
@@ -32,11 +33,42 @@ tabData << [name: 'members',    icon: 'icon_users', messageCode: 'spring.securit
                             labelCodeDefault='Address' value="${company?.address}"/>
             <s2ui:textFieldRow name='taxId' labelCode='company.taxId.label' bean="${company}"
                             labelCodeDefault='Tax ID' value="${company?.taxId}"/>
+            <tr class="prop">
+						<td class="name" valign="top">
+							<label for="policyKeyInterestMethod"><g:message code="company.policyKeyInterestMethod.label" default="Key Interest Method" /></label>
+						</td>
+						<td class="value " valign="top">
+							
+							<g:select name="policyKeyInterestMethod" value="${policyKeyInterestMethod}" optionKey="id" optionValue="key"
+							from="${[[id:Policy.VALUE_COMPOUND, key:message(code: 'policy.CompoundInterest.label', default: 'Compound Interest')], 
+							[id:Policy.VALUE_NON_COMPOUND, key:message(code: 'policy.NonCompoundInterest.label', default: 'Non Compound Interest')]]}" 
+							 />				
+						</td>
+					</tr>
+					
+					<tr class="prop">
+						<td class="name" valign="top">
+							<label for="policyKeyInterestRateLimit"><g:message code="company.policyKeyInterestRateLimit.label" default="Key Interest Rate Limit" /></label>
+						</td>
+						<td class="value " valign="top">
+							<input id="policyKeyInterestRateLimit" type="text" size="25" value="${policyKeyInterestRateLimit}" name="policyKeyInterestRateLimit">
+						</td>
+					</tr>
+					
+					<tr class="prop">
+						<td class="name" valign="top">
+							<label for="policyKeyCreditLine"><g:message code="company.policyKeyCreditLine.label" default="Key Credit Line" /></label>
+						</td>
+						<td class="value " valign="top">
+							<input id="policyKeyCreditLine" type="text" size="25" value="${policyKeyCreditLine}" name="policyKeyCreditLine">
+						</td>
+					</tr>
+					<tr><td>&nbsp;</td></tr>
 		</tbody>
 		</table>
 	</s2ui:tab>
 
-	<s2ui:tab name='users' height='150'>
+	<s2ui:tab name='users' height='200'>
 		<g:if test='${users?.empty}'>
 		<g:message code="spring.security.ui.company_no_users"/>
 		</g:if>
@@ -45,7 +77,7 @@ tabData << [name: 'members',    icon: 'icon_users', messageCode: 'spring.securit
 		</g:each>
 	</s2ui:tab>
 
-	<s2ui:tab name='members' height='150'>
+	<s2ui:tab name='members' height='200'>
 		<g:if test='${members?.empty}'>
 		<g:message code="spring.security.ui.company_no_members"/>
 		</g:if>
